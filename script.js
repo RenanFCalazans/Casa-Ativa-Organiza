d10d41b8137817c1691c312978b0da3d6e4da376
Renan Calazans <116785133+RenanFCalazans@users.noreply.github.com>
2025-05-22 21:12:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('cadastroForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Obrigado pelo cadastro! Em breve entraremos em contato.');
+  this.reset();
+});
